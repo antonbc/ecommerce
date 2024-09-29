291946c1eaee66bdce5fd0a3f69006ae226128d9
anton <125847709+antonbc@users.noreply.github.com.>
2024-09-30 00:25:17 +0800

    added initial navbar layout and design

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import { createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import NavigationBar from './components/NavigationBar'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <>
+      <NavigationBar/>
+    </>
   </StrictMode>,
 )
