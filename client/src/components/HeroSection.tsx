83f23327c17a22ff2fbfd9963ca2f71652914e56
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-21 20:14:24 +0800

    add: added styles for the container

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,9 +1,9 @@
 import ImageSlider from "./ui/herosection-ui/ImageSlider";
 function HeroSection(){
     return (
-        <>
+        <div className="w-full h-full">
             <ImageSlider/>
-        </>
+        </div>
     )
 }
 export default HeroSection;
