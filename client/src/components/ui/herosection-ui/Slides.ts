c1c89ed71c43b5c38fe438ba5d4c8f9a578759b3
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-21 20:48:50 +0800

    update: changed the images

--- client/src/components/ui/herosection-ui/Slides.ts
+++ client/src/components/ui/herosection-ui/Slides.ts
@@ -1,16 +1,20 @@
+import img1 from '../../../assets/images/hero-img-1.jpg';
+import img2 from '../../../assets/images/hero-img-2.jpg';
+
 const Slides = [
     {
-        url: '../../../assets/images/hero-img-1.jpg',
-        heading:`Gamer's Favorites. \n Best Sellers`
+        url: img1,
+        heading: `Gamer's Favorites. \n Best Sellers`
     },
     {
-        url: '../../../assets/images/hero-img-1.jpg',
-        heading:`Gamer's Favorites. \n Best Sellers`
+        url: img2,
+        heading: `Gamer's Favorites. \n Best Sellers`
     },
     {
-        url: '../../../assets/images/hero-img-1.jpg',
-        heading:`Gamer's Favorites. \n Best Sellers`
+        url: img1,
+        heading: `Gamer's Favorites. \n Best Sellers`
     }
-]
+];
+
 
 export default Slides;
