1e5e11fe0324ef846f7cc2cc17e32274b7d9e412
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-21 21:33:02 +0800

    add: added the dots for the image slider

--- client/src/components/ui/herosection-ui/ImageSlider.tsx
+++ client/src/components/ui/herosection-ui/ImageSlider.tsx
@@ -1,29 +1,51 @@
 import { useState } from "react";
 import Slides from "./Slides";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-
+import { GoDotFill } from "react-icons/go";
 
 function ImageSlider() {
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  function goToNextSlide(){
+  function goToNextSlide() {
     const isLastSlide = currentIndex === Slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex)
+    setCurrentIndex(newIndex);
   }
 
-  function goToPreviousSlide(){
+  function goToPreviousSlide() {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? Slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex)
+    setCurrentIndex(newIndex);
+  }
+
+  function goToSlide(slideIndex) {
+    setCurrentIndex(slideIndex);
   }
 
   return (
     <div className="relative h-full">
-      <FaChevronLeft className="p-2 z-1 top-1/2 left-2 absolute rounded-2xl text-4xl bg-slate-100 cursor-pointer" onClick={goToPreviousSlide}/>
-      <FaChevronRight className="p-2 z-1 top-1/2 right-2 absolute rounded-2xl text-4xl bg-slate-100 cursor-pointer" onClick={goToNextSlide}/>
-      <div className="w-full h-[85vh] bg-center bg-cover" style={{backgroundImage: `url(${Slides[currentIndex].url})`}}>
-
+      <FaChevronLeft
+        className="p-2 z-1 top-1/2 left-2 absolute rounded-3xl text-4xl bg-slate-100 cursor-pointer"
+        onClick={goToPreviousSlide}
+      />
+      <FaChevronRight
+        className="p-2 z-1 top-1/2 right-2 absolute rounded-3xl text-4xl bg-slate-100 cursor-pointer"
+        onClick={goToNextSlide}
+      />
+      <div
+        className="w-full h-[85vh] bg-center bg-cover"
+        style={{ backgroundImage: `url(${Slides[currentIndex].url})` }}
+      ></div>
+      <div className="absolute bottom-0 left-0 right-0 mx-auto  z-1 flex items-center justify-center ">
+        {Slides.map((_, slideIndex) => (
+          <GoDotFill
+            key={slideIndex}
+            className="text-3xl"
+            onClick={() => {
+              goToSlide(slideIndex);
+            }}
+          />
+        ))}
       </div>
     </div>
   );
