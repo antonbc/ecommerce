5116ee3104f840403307960a6b1c63d227bac2a1
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-21 16:09:10 +0800

    update: removed the useState in the image slider

--- client/src/components/ui/herosection-ui/ImageSlider.tsx
+++ client/src/components/ui/herosection-ui/ImageSlider.tsx
@@ -1,14 +1,4 @@
-import { useState } from "react";
-
 function ImageSlider() {
-  const [pageIndex, setPageIndex] = useState(0)
-
-  function handleBackPageClick(){
-    return setPageIndex(pageIndex - 1)
-  }
-  function handleNextPageClick(){
-    return setPageIndex(pageIndex + 1)
-  }
 
   return (
     <div className="relative h-full">
