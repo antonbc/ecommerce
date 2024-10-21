611ed3278ba3c16da8d159d65e9f44f956f05c0d
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-21 15:18:08 +0800

    add: added useState and its corresponding function to image slider

--- client/src/components/ui/herosection-ui/ImageSlider.tsx
+++ client/src/components/ui/herosection-ui/ImageSlider.tsx
@@ -1,8 +1,15 @@
 //import { useState } from "react";
 
 function ImageSlider() {
-  //const [imageIndex, setImageIndex] = useState(0)
+  const [pageIndex, setPageIndex] = useState(0)
 
+  function handleBackPageClick(){
+    return setPageIndex(pageIndex - 1)
+  }
+  function handleNextPageClick(){
+    return setPageIndex(pageIndex + 1)
+  }
+  
   return (
     <div className="">
       <img src="src/assets/images/heroimage1.jpg" alt="hero-image-1" className="w-full h-full object-cover"/>
