e5c7157e3a10fa00a6c1fdcc603f89141fa32334
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-23 11:38:12 +0800

    add: added featured section

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import NavigationBar from "../components/NavigationBar";
 import HeroSection from "../components/HeroSection";
+import CategoriesSection from "../components/FeaturedCategories";
 
 function HomePage() {
   return (
     <>
       <NavigationBar />
       <HeroSection />
+      <CategoriesSection />
     </>
   );
 }
