5d9ba886b3384338f1f5c0df52a793113697d86e
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-24 00:07:44 +0800

    fix: adjusted padding and margins

--- client/src/components/FeaturedCategories.tsx
+++ client/src/components/FeaturedCategories.tsx
@@ -2,13 +2,13 @@ import CategoriesCard from "./ui/featured-categories-ui/CategoriesCard";
 
 function CategoriesSection(){
     return (
-        <div className="mb-6">
+        <div className="mb-24">
             <div className="mx-auto text-neutral-300">
                 <div className="flex justify-between align-bottom w-full py-6 px-6 lg:px-8">
                     <h1 className="font-bold text-2xl">Featured Categories</h1>
                     <span className="underline underline-offset-2 self-end cursor-pointer font-semibold hover:text-neutral-100">See all categories</span>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 px-4 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-6 lg:px-8">
                     <CategoriesCard/>
                 </div>
             </div>
--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import HeroSlider from "./ui/herosection-ui/HeroSlider";
 function HeroSection(){
     return (
-        <div className="w-full h-full mb-6">
+        <div className="w-full h-full mb-10">
             <HeroSlider/>
         </div>
     )
