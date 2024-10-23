419792547f79f5dd24229b27c60545c03e4462da
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-24 00:01:55 +0800

    add: new arrivals section

--- client/src/components/NewArrivals.tsx
+++ client/src/components/NewArrivals.tsx
@@ -2,15 +2,13 @@ import NewArrivalsCard from "./ui/new-arrivals-ui/NewArrivalsCard";
 
 function NewArrivals(){
     return (
-        <div className="mb-6">
+        <div className="mb-10">
             <div className="mx-auto text-neutral-300">
                 <div className="flex justify-between align-bottom w-full py-6 px-6 lg:px-8">
                     <h1 className="font-bold text-2xl">New Arrivals</h1>
-                    <span className="underline underline-offset-2 self-end cursor-pointer font-semibold hover:text-neutral-100">View more →</span>
+                    <span className=" self-end cursor-pointer font-semibold text-blue-500 hover:underline">View more →</span>
                 </div>
-                <div className="">
-                    <NewArrivalsCard/>
-                </div>
+                <NewArrivalsCard/>
             </div>
         </div>
     )
