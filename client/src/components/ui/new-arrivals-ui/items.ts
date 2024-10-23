51cc6d2ae8efe71331ca71e812beb433c6423efc
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-24 00:08:38 +0800

    add: added products for new arrivals section

--- client/src/components/ui/new-arrivals-ui/items.ts
+++ client/src/components/ui/new-arrivals-ui/items.ts
@@ -1,5 +1,29 @@
-const NewArrivals = [
+import img1 from '../../../assets/svgs/ps5-dark.svg';
+import img2 from '../../../assets/svgs/iphone-dark.svg';
+import img3 from '../../../assets/svgs/apple-watch-dark.svg';
+import img4 from '../../../assets/svgs/ipad-dark.svg';
 
+const NewArrivals = [
+    {
+        title:'PlayStation5',
+        description:'Black and White version of the Ps5 is coming out.',
+        backgroundImage: img1
+    },
+    {
+        title:'Iphone 14',
+        description:'Black and White version of the Ps5 is coming out.',
+        backgroundImage: img2
+    },
+    {
+        title:'Apple Watch Series 8',
+        description:'Black and White version of the Ps5 is coming out.',
+        backgroundImage: img3
+    },
+    {
+        title:'Ipad Pro 11 inch',
+        description:'Black and White version of the Ps5 is coming out.',
+        backgroundImage: img4
+    }
 ]
 
 export default NewArrivals;
