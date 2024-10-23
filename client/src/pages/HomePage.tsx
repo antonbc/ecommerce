740ffd051d6ec0c81ba1c01d633e3da6d314442b
anton <125847709+antonbc@users.noreply.github.com.>
2024-10-24 00:04:35 +0800

    update: added best sellers, popular products, footer

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,14 +1,23 @@
 import NavigationBar from "../components/NavigationBar";
 import HeroSection from "../components/HeroSection";
 import CategoriesSection from "../components/FeaturedCategories";
+import NewArrivals from "../components/NewArrivals";
+import PopularProducts from "../components/PopularProducts";
+import BestSellers from "../components/BestSellers";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
     <div className="bg-gray-900">
       <NavigationBar />
       <HeroSection />
+      <PopularProducts/>
+      <BestSellers/>
+      <NewArrivals/>
+      <PopularProducts/>
+      <BestSellers/>
       <CategoriesSection />
-      
+      <Footer/>
     </div>
   );
 }
