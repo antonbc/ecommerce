60c6a5082438484322b7101e38752e73b750ecd5
anton <125847709+antonbc@users.noreply.github.com>
2024-10-24 21:23:40 +0800

    add: department page

---
+++ client/src/pages/DepartmentPage.tsx
@@ -0,0 +1,15 @@
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer";
+import DepartmentSection from "../components/DepartmentsSection";
+
+function DepartmentPage(){
+    return (
+        <div>
+            <NavigationBar/>
+            <DepartmentSection/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default DepartmentPage;
