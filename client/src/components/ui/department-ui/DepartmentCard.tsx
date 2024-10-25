7b8247563326019e132869dbdfc9bb74fa29401d
anton <125847709+antonbc@users.noreply.github.com>
2024-10-25 22:03:36 +0800

    fix: changed object-cover to object-contain so the svgs won't overflow

--- client/src/components/ui/department-ui/DepartmentCard.tsx
+++ client/src/components/ui/department-ui/DepartmentCard.tsx
@@ -4,26 +4,26 @@ function DepartmentCard() {
     return (
         <>
             {departments.map((department, index) => (
-                <div className="border border-gray-700 bg-gray-800" key={index}>
+                <div className="border border-gray-700 bg-gray-800 rounded-xl" key={index}>
                     <div className="grid grid-cols-2">
                         {department.subCategories.map((subCategory, subIndex) => (
                             <div
-                                className={`relative p-4 transition grid grid-rows-2 ${
-                                    subIndex === 0
-                                        ? "border-b-gray-700"
-                                        : subIndex === 1
-                                        ? "border-l-gray-700 border-b-gray-700"
-                                        : subIndex === 2
-                                        ? "border-r-gray-700 border-b-gray-700"
-                                        : "border-t-gray-700"
-                                }`}
+                            className={`relative p-4 transition grid grid-rows-2 h-full ${
+                                subIndex === 0
+                                    ? "border-r border-b border-gray-700"  // Top-left item
+                                    : subIndex === 1
+                                    ? "border-b border-gray-700"            // Top-right item
+                                    : subIndex === 2
+                                    ? "border-r border-gray-700"            // Bottom-left item
+                                    : ""                                     // Bottom-right item (no additional border)
+                            }`}
                                 key={subIndex}
                             >
-                                <div className="w-full h-32 mx-auto overflow-hidden">
+                                <div className="w-full h-32 mx-auto">
                                     <img
                                         src={subCategory.image}
                                         alt={subCategory.categoryName}
-                                        className="w-full h-full object-cover"
+                                        className="w-full h-full object-contain"
                                     />
                                 </div>
                                 <h3 className="text-white text-center">{subCategory.categoryName}</h3>
