d15e719005f5379da03213039a0136be30846045
anton <125847709+antonbc@users.noreply.github.com>
2024-10-27 23:29:53 +0800

    update: placed the all button and used an array for generating the nav links

--- client/src/components/ui/navbar-ui/NavigationLinks.tsx
+++ client/src/components/ui/navbar-ui/NavigationLinks.tsx
@@ -1,56 +1,39 @@
 import { NavLink } from "react-router-dom";
+import { IoMenu as Menu } from "react-icons/io5";
+
+const navLinks = [
+  { to: "/Department", label: "All", icon: <Menu className="mr-1 text-2xl" /> },
+  { to: "/Department", label: "Today's Deals" },
+  { to: "/Electronics", label: "Customer Service" },
+  { to: "/", label: "Gift Cards" },
+  { to: "/", label: "New Arrivals" },
+];
+
+type LinkStyleProps = {
+  isActive: boolean;
+  isPending: boolean;
+};
+
+const linkStyle = ({ isActive, isPending }: LinkStyleProps) => ({
+  fontWeight: isActive ? "semibold" : "",
+  color: isPending ? "text-neutral-200" : "",
+});
 
 function NavigationLinks() {
   return (
     <ul className="flex space-x-6">
-      <li>
-        <NavLink
-          to="/BestSeller"
-          className="hover:underline"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "",
-            color: isActive ? "red" : "",
-          })}
-        >
-          Best Sellers
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/CustomerService"
-          className="hover:underline"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "",
-            color: isActive ? "red" : "black",
-          })}
-        >
-          Customer Service
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/GiftCard"
-          className="hover:underline"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "",
-            color: isActive ? "red" : "black",
-          })}
-        >
-          Gift Card
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/TopDeals"
-          className="hover:underline"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "",
-            color: isActive ? "red" : "black",
-          })}
-        >
-          Top Deals
-        </NavLink>
-      </li>
+      {navLinks.map((link, index) => (
+        <li key={index}>
+          <NavLink
+            to={link.to}
+            className="flex items-center hover:underline"
+            style={linkStyle}
+          >
+            {link.icon}
+            {link.label}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 }
