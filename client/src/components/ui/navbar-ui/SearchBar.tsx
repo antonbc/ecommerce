ba34ec4070675560daa68159340dc05829716e6a
anton <125847709+antonbc@users.noreply.github.com>
2024-10-28 21:28:20 +0800

    update: margin for medium screen and blue color of search button

--- client/src/components/ui/navbar-ui/SearchBar.tsx
+++ client/src/components/ui/navbar-ui/SearchBar.tsx
@@ -3,7 +3,7 @@ import { FaChevronDown } from "react-icons/fa";
 
 function SearchBar() {
   return (
-    <div className="hidden md:flex items-center flex-grow mx-16 h-10">
+    <div className="hidden md:flex items-center flex-grow md:mx-12 lg:mx-16 h-10">
       <div className="flex items-center h-full">
         <button
           className="flex items-center gap-2 py-2 px-3 bg-gray-700  rounded-l-lg  border border-gray-600 h-full hover:bg-gray-600"
@@ -24,7 +24,7 @@ function SearchBar() {
 
       <button
         type="button"
-        className="p-2 h-full rounded-r-lg bg-blue-700 hover:bg-blue-800"
+        className="p-2 h-full rounded-r-lg bg-blue-600 hover:bg-blue-700"
         aria-label="Search"
       >
         <SearchIcon className="h-5 w-5" />
