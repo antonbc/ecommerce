f1d7a4bce7095edd5c93dad50e66fb1f95808e31
anton <125847709+antonbc@users.noreply.github.com>
2024-10-31 00:00:23 +0800

    update: added functionality and design

--- client/src/components/ui/product-card-overview-ui/SortButton.tsx
+++ client/src/components/ui/product-card-overview-ui/SortButton.tsx
@@ -6,10 +6,9 @@ function SortButton() {
   const [isOpen, setIsOpen] = useState(false);
   const [sortOption, setSortOption] = useState("Sort")
 
-  function toggleSortOption() {
-    if (isOpen) {
-      return;
-    } else return setIsOpen(true);
+  function toggleSortOption(option: string) {
+    setSortOption(option)
+    setIsOpen(!isOpen)
   }
 
   const sortOptions = [
@@ -24,17 +23,21 @@ function SortButton() {
     <div className="relative">
       <button
         type="button"
-        className="flex items-center gap-2 bg-gray-800 border border-gray-700 rounded-md p-2 text-gray-400 text-sm"
+        className="flex items-center gap-2 bg-gray-800 hover:bg-gray-700 hover:text-gray-300 border border-gray-700 rounded-md p-2 text-gray-400 text-sm"
         onClick={() => setIsOpen(!isOpen)}
       >
         <FaSortAlphaUpAlt />
-        <span className="font-semibold">Sort</span>
+        <span className="font-semibold">{sortOption}</span>
         <FaAngleDown />
       </button>
 
         {isOpen && (
-            <div className="absolute">
-
+            <div className="absolute mx-auto mt-2 flex flex-col items-start font-medium text-sm bg-gray-800 text-gray-400 border border-gray-700 rounded-md p-1">
+                {sortOptions.map((option, index) => (
+                    <button type="button" key={index} onClick={() => toggleSortOption(option)} className="hover:bg-gray-700 rounded-md p-1">
+                        {option}
+                    </button>
+                ))}
             </div>
         )}
     </div>
