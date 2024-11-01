2ecb62a4b4c601cc56121cfa2fad998959e4bac2
anton <125847709+antonbc@users.noreply.github.com>
2024-11-01 23:15:16 +0800

    add: electronics department page

---
+++ client/src/pages/ElectronicsPage.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import NavigationBar from "../components/NavigationBar";
+import ProductCardOverview from "../components/ProductCardOverview";
+function ElectronicsPage(){
+    return (
+        <div className="bg-gray-900 min-w-fit">
+            <NavigationBar/>
+            <ProductCardOverview/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default ElectronicsPage;
