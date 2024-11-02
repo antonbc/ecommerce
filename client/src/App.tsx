edb2d334a87c359e9c23a87ed5f74981a43b9701
anton <125847709+antonbc@users.noreply.github.com>
2024-11-02 23:22:51 +0800

    add: added new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,30 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DepartmentPage from "./pages/DepartmentPage";
+import ElectronicsPage from "./pages/ElectronicsPage";
+import MobilePhonesPage from "./pages/MobilePhonesPage"; // New component
 
 const router = createBrowserRouter([
     {
-      path: '/',
-      element: <HomePage />,
+        path: '/',
+        element: <HomePage />,
     },
     {
-      path: '/Department',
-      element: <DepartmentPage />,
+        path: '/department',
+        element: <DepartmentPage />,
     },
-  ])
+    {
+        path: '/department/electronics',
+        element: <ElectronicsPage />,
+    },
+    {
+        path: '/department/electronics/mobile-phones',
+        element: <MobilePhonesPage />, // New page for mobile phones
+    },
+]);
 
-function App(){
-    return (
-        <RouterProvider router={router} />
-    )
+function App() {
+    return <RouterProvider router={router} />;
 }
 
 export default App;
