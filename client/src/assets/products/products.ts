62cdd84d6fd5628ca6611389ed4f1b742d36a851
anton <125847709+antonbc@users.noreply.github.com>
2024-11-03 23:28:46 +0800

    add: Added more products

--- client/src/assets/products/products.ts
+++ client/src/assets/products/products.ts
@@ -269,6 +269,198 @@ const products = [
         rating: 3.5,
         reviewCount: 432
     },
+    {
+        id: 33,
+        imageUrl: img1,
+        name: 'PlayStation 5',
+        price: '750',
+        rating: 4.5,
+        reviewCount: 30
+    },
+    {
+        id: 34,
+        imageUrl: img2,
+        name: 'Iphone 15 Pro Max',
+        price: '1199',
+        rating: 5,
+        reviewCount: 89
+    },
+    {
+        id: 35,
+        imageUrl: img3,
+        name: 'Apple Watch Series 8',
+        price: '399',
+        rating: 3.5,
+        reviewCount: 126
+    },
+    {
+        id: 36,
+        imageUrl: img4,
+        name: 'Ipad Pro 11-inch',
+        price: '1299',
+        rating: 4,
+        reviewCount: 432
+    },
+    {
+        id: 37,
+        imageUrl: img5,
+        name: 'Macbook Pro M3',
+        price: '2150',
+        rating: 5,
+        reviewCount: 40
+    },
+    {
+        id: 38,
+        imageUrl: img6,
+        name: 'Xbox Series X',
+        price: '750',
+        rating: 5,
+        reviewCount: 35
+    },
+    {
+        id: 39,
+        imageUrl: img7,
+        name: 'Ipad Air 2023',
+        price: '950',
+        rating: 3.5,
+        reviewCount: 23
+    },
+    {
+        id: 40,
+        imageUrl: img8,
+        name: 'PlayStation 5 Controller',
+        price: '50',
+        rating: 4.5,
+        reviewCount: 72
+    },
+    {
+        id: 41,
+        imageUrl: img9,
+        name: 'PlayStation 5',
+        price: '750',
+        rating: 4.5,
+        reviewCount: 30
+    },
+    {
+        id: 42,
+        imageUrl: img10,
+        name: 'Iphone 15 Pro Max',
+        price: '1199',
+        rating: 5,
+        reviewCount: 89
+    },
+    {
+        id: 43,
+        imageUrl: img11,
+        name: 'Apple Watch Series 8',
+        price: '399',
+        rating: 5,
+        reviewCount: 126
+    },
+    {
+        id: 44,
+        imageUrl: img12,
+        name: 'Ipad Pro 11-inch',
+        price: '1299',
+        rating: 3.5,
+        reviewCount: 432
+    },
+    {
+        id: 45,
+        imageUrl: img1,
+        name: 'PlayStation 5',
+        price: '750',
+        rating: 4.5,
+        reviewCount: 30
+    },
+    {
+        id: 46,
+        imageUrl: img2,
+        name: 'Iphone 15 Pro Max',
+        price: '1199',
+        rating: 5,
+        reviewCount: 89
+    },
+    {
+        id: 47,
+        imageUrl: img3,
+        name: 'Apple Watch Series 8',
+        price: '399',
+        rating: 3.5,
+        reviewCount: 126
+    },
+    {
+        id: 48,
+        imageUrl: img4,
+        name: 'Ipad Pro 11-inch',
+        price: '1299',
+        rating: 4,
+        reviewCount: 432
+    },
+    {
+        id: 49,
+        imageUrl: img5,
+        name: 'Macbook Pro M3',
+        price: '2150',
+        rating: 5,
+        reviewCount: 40
+    },
+    {
+        id: 50,
+        imageUrl: img6,
+        name: 'Xbox Series X',
+        price: '750',
+        rating: 5,
+        reviewCount: 35
+    },
+    {
+        id: 51,
+        imageUrl: img7,
+        name: 'Ipad Air 2023',
+        price: '950',
+        rating: 3.5,
+        reviewCount: 23
+    },
+    {
+        id: 52,
+        imageUrl: img8,
+        name: 'PlayStation 5 Controller',
+        price: '50',
+        rating: 4.5,
+        reviewCount: 72
+    },
+    {
+        id: 53,
+        imageUrl: img9,
+        name: 'PlayStation 5',
+        price: '750',
+        rating: 4.5,
+        reviewCount: 30
+    },
+    {
+        id: 54,
+        imageUrl: img10,
+        name: 'Iphone 15 Pro Max',
+        price: '1199',
+        rating: 5,
+        reviewCount: 89
+    },
+    {
+        id: 55,
+        imageUrl: img11,
+        name: 'Apple Watch Series 8',
+        price: '399',
+        rating: 5,
+        reviewCount: 126
+    },
+    {
+        id: 56,
+        imageUrl: img12,
+        name: 'Ipad Pro 11-inch',
+        price: '1299',
+        rating: 3.5,
+        reviewCount: 432
+    }    
 ];
 
 export default products;
