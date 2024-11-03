c15061df7afc8cc492e5c0f8cd887affc4ea5f1d
anton <125847709+antonbc@users.noreply.github.com>
2024-11-03 23:24:37 +0800

    update: Changed text color from neutral to gray

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -6,7 +6,7 @@ import NavigationLinks from "./ui/navbar-ui/NavigationLinks";
 
 function NavigationBar() {
   return (
-    <nav className="z-50 top-0 sticky px-6 md:px-0 bg-gray-800 text-neutral-200 antialiased ">
+    <nav className="z-50 top-0 sticky px-6 md:px-0 bg-gray-800 text-gray-200 antialiased">
       <div className="mx-auto max-w-screen-xl sm:mx-4 md:mx-8 font-medium border-b border-gray-600">
         <div className="py-2 flex justify-between items-center w-full">
           <Logo />
