420fdffcf1eb510a0d5cf8b521d2fed4a192843c
anton <125847709+antonbc@users.noreply.github.com>
2024-11-04 23:21:12 +0800

    add: Added a function which format's the department path

--- client/src/components/ui/product-card-overview-ui/BreadCrumbs.tsx
+++ client/src/components/ui/product-card-overview-ui/BreadCrumbs.tsx
@@ -6,11 +6,18 @@ function BreadCrumbs() {
   const { pathname } = useLocation();
   const pathnames = pathname.split("/").filter((pathname) => pathname !== "");
 
+  function convertToTitle(pathname : string) {
+    return pathname.replace(/-/g, ' ') // Replace hyphens with spaces
+                   .split(' ')
+                   .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize words
+                   .join(' ');
+  }
+
   return (
-    <div className="bg-gray-900 p-2">
+    <div className="bg-gray-900">
       <nav className="flex items-center gap-2 text-gray-500 font-semibold text-sm">
         {/* Always show a link to Home */}
-          <Link to="/" className="flex items-center gap-2 hover:text-gray-200">
+          <Link to="/" className="flex items-center gap-2 hover:text-gray-300">
            <AiFillHome className="" />
             Home
           </Link>
@@ -19,10 +26,10 @@ function BreadCrumbs() {
           <>
             <MdOutlineKeyboardArrowRight className="text-2xl" />
             {index !== 0 ? (
-              <span>{pathname[0].toUpperCase() + pathname.slice(1)}</span>
+              <span className="text-gray-300">{convertToTitle(pathname)}</span>
             ) : (
               <Link to={`/${pathname}`} className="hover:text-gray-300">
-                {pathname[0].toUpperCase() + pathname.slice(1)}
+                {convertToTitle(pathname)}
               </Link>
             )}
           </>
