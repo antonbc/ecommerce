1f1ec2fc3c36f6da95e8140bce8ce9691a37a797
anton <125847709+antonbc@users.noreply.github.com>
2024-11-04 22:26:41 +0800

    "fix: Changed text color"

--- client/src/components/DepartmentsSection.tsx
+++ client/src/components/DepartmentsSection.tsx
@@ -3,10 +3,10 @@ function DepartmentSection(){
     return(
         <div className="bg-gray-900">
             <div>
-                <div className="flex justify-between align-bottom w-full py-6 px-6 lg:px-8">
-                    <h1 className="font-bold text-2xl">Browse Departments</h1>
+                <div className="flex justify-between align-bottom w-full p-6 lg:px-8">
+                    <h1 className="font-bold text-2xl tracking-wide text-gray-200">Browse Departments</h1>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 lg:px-8 pb-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-6 lg:px-8 pb-16">
                     <DepartmentCard />
                 </div>
             </div>
