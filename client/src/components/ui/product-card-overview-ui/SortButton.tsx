84bacfea3717260acd492219504336d026b1affe
anton <125847709+antonbc@users.noreply.github.com>
2024-11-04 23:23:05 +0800

    update: changed the z-index

--- client/src/components/ui/product-card-overview-ui/SortButton.tsx
+++ client/src/components/ui/product-card-overview-ui/SortButton.tsx
@@ -4,11 +4,11 @@ import { FaAngleDown } from "react-icons/fa6";
 
 function SortButton() {
   const [isOpen, setIsOpen] = useState(false);
-  const [sortOption, setSortOption] = useState("Sort")
+  const [sortOption, setSortOption] = useState("Sort");
 
   function toggleSortOption(option: string) {
-    setSortOption(option)
-    setIsOpen(!isOpen)
+    setSortOption(option);
+    setIsOpen(!isOpen);
   }
 
   const sortOptions = [
@@ -20,7 +20,7 @@ function SortButton() {
     "Best Sellers",
   ];
   return (
-    <div className="relative">
+    <div className="relative z-20">
       <button
         type="button"
         className="flex items-center gap-2 bg-gray-800 hover:bg-gray-700 hover:text-gray-300 border border-gray-700 rounded-md p-2 text-gray-400 text-sm"
@@ -31,15 +31,20 @@ function SortButton() {
         <FaAngleDown />
       </button>
 
-        {isOpen && (
-            <div className="absolute mx-auto mt-2 flex flex-col font-base text-sm bg-gray-800 text-gray-400 border border-gray-700 rounded-md p-1">
-                {sortOptions.map((option, index) => (
-                    <button type="button" key={index} onClick={() => toggleSortOption(option)} className="hover:bg-gray-700 hover:text-gray-300 rounded-md py-1 px-2 w-full text-start">
-                        {option}
-                    </button>
-                ))}
-            </div>
-        )}
+      {isOpen && (
+        <div className="absolute right-0 mx-auto min-w-max mt-2 flex flex-col font-base text-sm bg-gray-800 text-gray-400 border border-gray-700 rounded-md p-1">
+          {sortOptions.map((option, index) => (
+            <button
+              type="button"
+              key={index}
+              onClick={() => toggleSortOption(option)}
+              className="hover:bg-gray-700 hover:text-gray-300 rounded-md py-1 px-2 w-full text-start"
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
