74d8453c357d7b082e64752312bef2abf84412dc
anton <125847709+antonbc@users.noreply.github.com>
2024-11-05 20:55:32 +0800

    add: Added search bar for categories and enlarged the heading names

--- client/src/components/ui/product-card-overview-ui/FilterSideBar.tsx
+++ client/src/components/ui/product-card-overview-ui/FilterSideBar.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import { IoSearchSharp as SearchIcon } from "react-icons/io5";
+
 function FilterSideBar() {
   const [categories] = useState([
     "Apple",
@@ -36,13 +38,15 @@ function FilterSideBar() {
     "Over $1000",
   ];
 
-  function renderStars(rating : number) {
+  function renderStars(rating: number) {
     const stars = [];
     for (let i = 1; i <= 5; i++) {
       stars.push(
         <FaStar
           key={i}
-          className={i <= rating ? "text-yellow-500 text-sm" : "text-gray-400 text-sm"}
+          className={
+            i <= rating ? "text-yellow-500 text-sm" : "text-gray-400 text-sm"
+          }
         />
       );
     }
@@ -50,21 +54,32 @@ function FilterSideBar() {
   }
 
   return (
-    <div className="border border-gray-700 rounded-lg bg-gray-800 shadow-2xl h-full p-2 w-60 md:w-72 lg:w-80 space-y-4">
-      <h3 className="font-semibold text-gray-300 text-lg">Filter</h3>
+    <div className="border border-gray-700 rounded-lg bg-gray-800 shadow-2xl h-full p-2 w-60 lg:w-72 space-y-4">
+      <div className="flex flex-col px-2">
+        <h4 className="font-semibold text-gray-300 text-xl">Categories</h4>
+        <div className="flex gap-2 my-2 bg-gray-700 border border-gray-600 rounded-lg p-2 relative">
+          <div>
+            <SearchIcon className="text-lg text-gray-400" />
+          </div>
 
-      <div className="flex flex-col border-t border-t-gray-700">
-        <h4 className="font-semibold text-gray-300">Categories</h4>
+          <input
+            type="text"
+            placeholder="Search for categories"
+            className="bg-transparent focus:outline-none text-gray-300 text-sm"
+          />
+        </div>
         {categories.map((category, index) => (
           <label key={index} className="space-x-2">
             <input type="checkbox" name={category} id={category} />
-            <span className="text-gray-400 text-sm font-medium">{category}</span>
+            <span className="text-gray-400 text-sm font-medium">
+              {category}
+            </span>
           </label>
         ))}
       </div>
 
-      <div className="flex flex-col gap-1 border-t border-t-gray-700">
-        <h4 className="font-semibold text-gray-300">Rating</h4>
+      <div className="flex flex-col gap-1 border-t border-t-gray-700 px-2">
+        <h4 className="font-semibold text-gray-300 text-xl">Rating</h4>
         <div className="flex flex-col gap-1">
           {ratings.map((rating, index) => (
             <label key={index} className="flex items-center gap-2">
@@ -79,19 +94,28 @@ function FilterSideBar() {
         </div>
       </div>
 
-      <div className="flex flex-col border-t border-t-gray-700 max-w-fit">
-        <h4 className="font-semibold text-gray-300">Price</h4>
+      <div className="flex flex-col border-t border-t-gray-700 px-2">
+        <h4 className="font-semibold text-gray-300 text-xl">Price</h4>
         <div className="flex flex-col space-y-1">
           {priceRanges.map((range, index) => (
             <label key={index} className="flex items-center space-x-2">
-              <input type="checkbox" name={`price-${index}`} id={`price-${index}`} />
+              <input
+                type="checkbox"
+                name={`price-${index}`}
+                id={`price-${index}`}
+              />
               <span className="text-gray-400 text-sm font-medium">{range}</span>
             </label>
           ))}
         </div>
         <div className="flex gap-1 mt-2">
           <div className="flex flex-col w-1/2">
-            <label htmlFor="priceFrom" className="text-gray-400 text-sm font-medium">From</label>
+            <label
+              htmlFor="priceFrom"
+              className="text-gray-400 text-sm font-medium"
+            >
+              From
+            </label>
             <input
               type="number"
               id="priceFrom"
@@ -101,7 +125,12 @@ function FilterSideBar() {
             />
           </div>
           <div className="flex flex-col w-1/2">
-            <label htmlFor="priceTo" className="text-gray-400 text-sm font-medium">To</label>
+            <label
+              htmlFor="priceTo"
+              className="text-gray-400 text-sm font-medium"
+            >
+              To
+            </label>
             <input
               type="number"
               id="priceTo"
@@ -113,8 +142,8 @@ function FilterSideBar() {
         </div>
       </div>
 
-      <div className="flex flex-col border-t border-t-gray-700">
-        <h4 className="font-semibold text-gray-300">Brand</h4>
+      <div className="flex flex-col border-t border-t-gray-700 px-2">
+        <h4 className="font-semibold text-gray-300 text-xl">Brand</h4>
         {brands.map((brand, index) => (
           <label key={index} className="space-x-2">
             <input type="checkbox" name={brand} id={brand} />
