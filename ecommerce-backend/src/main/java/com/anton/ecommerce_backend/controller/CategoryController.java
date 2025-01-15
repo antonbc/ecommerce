238674bdc96ea1892e3bd12c13629ca185abc30d
anton <125847709+antonbc@users.noreply.github.com>
2025-01-15 22:09:50 +0800

    Update: Modified to follow loose coupling

--- ecommerce-backend/src/main/java/com/anton/ecommerce_backend/controller/CategoryController.java
+++ ecommerce-backend/src/main/java/com/anton/ecommerce_backend/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.anton.ecommerce_backend.controller;
 
 import com.anton.ecommerce_backend.model.Category;
+import com.anton.ecommerce_backend.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,16 +10,20 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category){
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully from createCategory";
     }
 }
