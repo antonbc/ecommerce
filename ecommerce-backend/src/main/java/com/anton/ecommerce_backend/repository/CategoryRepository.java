234b40b095ed5f6bdcc35269258f94835539b77a
anton <125847709+antonbc@users.noreply.github.com>
2025-01-18 23:43:10 +0800

    Update: Used JpaRepository for persistence

---
+++ ecommerce-backend/src/main/java/com/anton/ecommerce_backend/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.anton.ecommerce_backend.repository;
+
+import com.anton.ecommerce_backend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
