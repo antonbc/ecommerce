c384ed3bb6ec9f39b16befae4f110bfc5043ba01
anton <125847709+antonbc@users.noreply.github.com>
2025-01-21 23:11:09 +0800

    Added new methods

--- ecommerce-backend/src/main/java/com/anton/ecommerce_backend/service/ProductService.java
+++ ecommerce-backend/src/main/java/com/anton/ecommerce_backend/service/ProductService.java
@@ -1,4 +1,13 @@
 package com.anton.ecommerce_backend.service;
 
+import com.anton.ecommerce_backend.model.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProductById(Long productId);
+    void createProduct(Product product);
+    String deleteProduct(Long productId);
+    Product updateProduct(Product updatedProduct, Long productId);
 }
