e50e184657eb545d6cb68558859341f930bbc539
Anton Chio <antonchio07@gmail.com>
2025-03-19 22:26:35 +0800

    feat(custom exceptions): created a custom global exception and resource not found exception response

---
+++ ecommerce-backend/src/main/java/com/anton/ecommerce_backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,25 @@
+package com.anton.ecommerce_backend.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    String resourceName;
+    String field;
+    String fieldName;
+    Long fieldId;
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String s, String resourceName, String field, String fieldName) {
+        super(String.format("%s not found with %s: %s", resourceName, field, fieldName));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldName = fieldName;
+    }
+
+    public ResourceNotFoundException(String s, String resourceName, String field, Long fieldId) {
+        super(String.format("%s not found with %s: %d", resourceName, field, fieldId));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldId = fieldId;
+    }
+}
